098237820cf82f55f109f397a65fd1908198ddfd
pabloconca <pabloconcasanchez@gmail.com>
2022-10-03 17:48:11 +0200

    Agregar archivos de proyecto.

---
+++ Opacidad/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opacidad
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void img_MouseIn(Object sender, DependencyPropertyChangedEventArgs e)
+        {
+            Image imagen = (Image)sender;
+            if (imagen.Opacity == 1)
+                imagen.Opacity = 0.5;
+            else imagen.Opacity = 1;
+        }
+
+    }
+    
+}
